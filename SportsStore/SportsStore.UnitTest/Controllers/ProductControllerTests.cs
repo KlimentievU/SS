4ebb136e62b35ea35481d86b1a471cb56e8585c1
klimentiev <KlimentievU@yandex.ua>
2016-08-24 20:41:36 +0300

    ss

---
+++ SportsStore/SportsStore.UnitTest/Controllers/ProductControllerTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportsStore.WebUI.Controllers;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
+
+namespace SportsStore.WebUI.Controllers.Tests
+{
+    [TestClass()]
+    public class ProductControllerTests
+    {
+        [TestMethod()]
+        public void Can_Paginate_tTest()
+        {
+            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+            mock.Setup(m=>m.Products).Returns(new Product[]
+            {
+                new Product() { ProductID = 1, Name = "p1"},
+                new Product() { ProductID = 2, Name = "p2"},
+                new Product() { ProductID = 3, Name = "p3"},
+                new Product() { ProductID = 4, Name = "p4"},
+                new Product() { ProductID = 5, Name = "p5"}
+            }.AsQueryable());
+             
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            IEnumerable<Product> result = (IEnumerable<Product>) controller.List(2).Model;
+
+            Product[] prodArray = result.ToArray();
+            Assert.IsTrue(prodArray.Length ==2);
+            Assert.AreEqual(prodArray[0].Name, "p4");
+            Assert.AreEqual(prodArray[1].Name,"p5");
+        }
+
+        [TestMethod]
+        public void Can_Generate_Page_Links()
+        {
+
+            HtmlHelper myHelper = null;
+            // Arrange - create PagingInfo data
+            PagingInfo pagingInfo = new PagingInfo
+            {
+                CurrentPage = 2,
+                TotalItems = 28,
+                ItemsPerPage = 10
+            };
+            // Arrange - set up the delegate using a lambda expression
+            Func<int, string> pageUrlDelegate = i => "Page" + i;
+            // Act
+            MvcHtmlString result = myHelper.PageLinks(pagingInfo, pageUrlDelegate);
+            // Assert
+            Assert.AreEqual(result.ToString(), @"<a href=""Page1"">1</a>"
+            + @"<a class=""selected"" href=""Page2"">2</a>"
+            + @"<a href=""Page3"">3</a>");
+        }
+
+    }
+}
---
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+using System.Web.Mvc;
+using Moq;
+using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.Domain.Concrete;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private IKernel ninjetKernel;
+
+        public NinjectControllerFactory()
+        {
+            ninjetKernel = new StandardKernel();
+            AddBindings();
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            return controllerType == null
+                ? null
+                : (IController) ninjetKernel.Get(controllerType);
+        }
+
+        private void AddBindings()
+        {
+            /*Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+
+            mock.Setup(m => m.Products).Returns(new List<Product>
+            {
+                new Product() {Name = "Football",Price = 25},
+                new Product() {Name = "Surf board", Price = 179},
+                new Product() {Name = "Running shoes", Price = 95},
+            }.AsQueryable());
+            ninjetKernel.Bind<IProductsRepository>().ToConstant(mock.Object);*/
+            ninjetKernel.Bind<IProductsRepository>().To<EFProductRepositoty>();
+        }
+    }
+}
